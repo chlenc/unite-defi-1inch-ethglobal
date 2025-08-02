eda73f079d700b79eb6e4df43e98628c5148132a
alexey <alexnagorny.an@gmail.com>
2025-08-02 16:16:34 +0100

    Updates

---
+++ assets/arrow-left-Ox7Qzhgh.js
@@ -0,0 +1,8 @@
+import{b as o}from"./core-uhPu0QGJ.js";import"./index-CwqTxa1L.js";import"./index.es-C9KAeubT.js";import"./sha3-DG6CBcqs.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M13 7.99a1 1 0 0 1-1 1H4.4l2.46 2.46a1 1 0 1 1-1.41 1.41L1.29 8.7a1 1 0 0 1 0-1.41L5.46 3.1a1 1 0 0 1 1.41 1.42L4.41 6.99H12a1 1 0 0 1 1 1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{p as arrowLeftSvg};
--- assets/chevron-bottom-BGOBqimF.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-Cj2RpoCb.js";import"./index-uJCF4F6V.js";import"./index.es-q72MQ1C7.js";import"./sha3-DG6CBcqs.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M1.46 4.96a1 1 0 0 1 1.41 0L8 10.09l5.13-5.13a1 1 0 1 1 1.41 1.41l-5.83 5.84a1 1 0 0 1-1.42 0L1.46 6.37a1 1 0 0 1 0-1.41Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{p as chevronBottomSvg};
